4e920648feadb4da454e778717ec61c4a8577e2f
zoranbogoevski <zoran@orangemelos.com>
2023-04-11 14:28:40 +0200

    remove unused models

--- Modules/Core/Models/Permission.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Modules\Core\Models;
-
-class Permission extends \Spatie\Permission\Models\Permission
-{
-    public $guard_name = 'api';
-
-}
--- Modules/Core/Models/Role.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Modules\Core\Models;
-
-class Role extends \Spatie\Permission\Models\Role
-{
-    public $guard_name = 'api';
-
-}
